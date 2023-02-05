87945570d97aea34ed42acb2746e1b03166121b2
Ke Bai <35400185+CrazyBoyM@users.noreply.github.com>
2023-02-06 02:26:25 +0800

    add support for safetensors and ckpt

--- merge.py
+++ merge.py
@@ -2,39 +2,37 @@ import os
 import argparse
 import torch
 from tqdm import tqdm
+from safetensors.torch import load_file, save_file
 
 parser = argparse.ArgumentParser(description="Merge two models")
 parser.add_argument("model_0", type=str, help="Path to model 0")
 parser.add_argument("model_1", type=str, help="Path to model 1")
 parser.add_argument("--alpha", type=float, help="Alpha value, optional, defaults to 0.5", default=0.5, required=False)
-parser.add_argument("--output", type=str, help="Output file name, without extension", default="merged", required=False)
+parser.add_argument("--dump_path", type=str, default=None, help="Path to the output file.", required=False)
 parser.add_argument("--device", type=str, help="Device to use, defaults to cpu", default="cpu", required=False)
 parser.add_argument("--without_vae", action="store_true", help="Do not merge VAE", required=False)
 
 args = parser.parse_args()
 
+def load_weights(path, device):
+  if path.endswith(".safetensors"):
+      weights = load_file(path, device)
+  else:
+      weights = torch.load(path, device)
+      weights = weights["state_dict"] if "state_dict" in weights else weights
+  
+  return weights
+
+def save_weights(weights, path):
+  if path.endswith(".safetensors"):
+      save_file(weights, path)
+  else:
+      torch.save({"state_dict": weights}, path) 
+        
 device = args.device
-model_0 = torch.load(args.model_0, map_location=device)
-model_1 = torch.load(args.model_1, map_location=device)
-theta_0 = model_0["state_dict"]
-theta_1 = model_1["state_dict"]
+theta_0 = load_weights(args.model_0, map_location=device)
+theta_1 = load_weights(args.model_1, map_location=device)
 alpha = args.alpha
-
-output_file = f'{args.output}-{str(alpha)[2:] + "0"}.ckpt'
-
-# check if output file already exists, ask to overwrite
-if os.path.isfile(output_file):
-    print("Output file already exists. Overwrite? (y/n)")
-    while True:
-        overwrite = input()
-        if overwrite == "y":
-            break
-        elif overwrite == "n":
-            print("Exiting...")
-            exit()
-        else:
-            print("Please enter y or n")
-
 
 for key in tqdm(theta_0.keys(), desc="Stage 1/2"):
     # skip VAE model parameters to get better results(tested for anime models)
@@ -51,6 +49,13 @@ for key in tqdm(theta_1.keys(), desc="Stage 2/2"):
 
 print("Saving...")
 
-torch.save({"state_dict": theta_0}, output_file)
+if args.dump_path is not None:
+  save_weights(theta_0, args.dump_path)
+else:
+  weights0_name = os.path.basename(args.model_0)
+  weights1_name = os.path.basename(args.model_1)
+  merged_name = f"{weights0_name}_{weights1_name}.safetensors"
+  dump_path = os.path.join(os.path.dirname(args.model_0), merged_name)
+  save_weights(theta_0, dump_path)
 
 print("Done!")
